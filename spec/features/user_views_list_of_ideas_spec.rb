5231c4fdc6d91844efead8f4a8df3dbb32668097
ToniRib <tonimarierib@gmail.com>
2016-03-21 13:28:04 -0600

    Write feature test for viewing a list of ideas

---
+++ spec/features/user_views_list_of_ideas_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "User views list of ideas", type: :feature do
+  scenario "when there are two ideas in the database" do
+    idea1 = Idea.create(title: "Idea1", body: "Body1", quality: "swill")
+    idea2 = Idea.create(title: "Idea2", body: "Body2", quality: "genius")
+
+    visit root_path
+
+    expect(page).to have_content(idea1.title)
+    expect(page).to have_content(idea1.body)
+    expect(page).to have_content(idea1.quality)
+    expect(page).to have_content(idea2.title)
+    expect(page).to have_content(idea2.body)
+    expect(page).to have_content(idea2.quality)
+  end
+end
