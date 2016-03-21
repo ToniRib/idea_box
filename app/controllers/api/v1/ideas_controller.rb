ea97e638639df1317f0794d69b3bc44b0e547d13
ToniRib <tonimarierib@gmail.com>
2016-03-21 15:45:13 -0600

    Add text boxes and button for adding a new idea

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::IdeasController < Api::V1::BaseController
   def index
     respond_with Idea.newest_to_oldest
   end
+
+  def create
+    respond_with Idea.create(idea_params)
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
+  end
 end
