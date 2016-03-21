362054c1c3524e726864a737cb7b9ebefbd9dc55
ToniRib <tonimarierib@gmail.com>
2016-03-21 15:34:57 -0600

    Add simple Seeds.rb file with two ideas preloaded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def self.start
+    new.generate
+  end
+
+  def generate
+    create_ideas
+  end
+
+  def create_ideas
+    Idea.create(title: "Drink Beer",
+                body:  "I think that in order to finish this project, I need " \
+                       "to drink at least 2 IPAs per day or else I will fail",
+                quality: "genius")
+    Idea.create(title: "Finish This Project",
+                body:  "Complete this project on time by Thursday morning")
+  end
+end
+
+Seed.start
