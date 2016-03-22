8db54a36c7d619540821070949529bf646022cb2
ToniRib <tonimarierib@gmail.com>
2016-03-22 13:26:24 -0600

    Add (skipped) feature test for sorting by ascending quality

--- spec/features/user_sorts_ideas_spec.rb
+++ spec/features/user_sorts_ideas_spec.rb
@@ -23,6 +23,24 @@ RSpec.describe "User sorts ideas", type: :feature do
   end
 
   xscenario "by ascending quality", js: true do
+    idea1 = create(:idea, title: 'idea1', quality: 'plausible')
+    idea2 = create(:idea, title: 'idea2', quality: 'swill')
+    idea3 = create(:idea, title: 'idea3', quality: 'genius')
 
+    visit root_path
+    click_on "Sort by Quality"
+    click_on "Sort by Quality"
+
+    within ".idea:nth-of-type(1)" do
+      expect(page).to have_content(idea2.title)
+    end
+
+    within ".idea:nth-of-type(2)" do
+      expect(page).to have_content(idea1.title)
+    end
+
+    within ".idea:nth-of-type(3)" do
+      expect(page).to have_content(idea3.title)
+    end
   end
 end
